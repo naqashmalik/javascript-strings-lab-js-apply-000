79352389b3dfdec237d684caa92c5e400759cb18
beloved-processor-5901 <naqashmalik139@gmail.com>
2018-09-23 19:07:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var greeting = "Hello, everybody!";
+=======
+var greeting = "Hello, World";
+>>>>>>> ece9aa8f24f1143e8064d7622ffbb09dc1389cb3
 
 var specialGuest = "Neil deGrasse Tyson"
 
